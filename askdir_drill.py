52340623b5526cf0f09f70042b424b5c49437e1e
Austin Reeves <dogman_95@yahoo.com>
2020-01-27 17:50:43 -0800

    update askdir_drill.py

--- askdir_drill.py
+++ askdir_drill.py
@@ -10,7 +10,6 @@ class ParentWindow(Frame):
         self.master = master
         self.master.minsize(500,80)
         self.master.maxsize(500,80)
-        center_window(self, 550,200)
         self.master.title("Choose Directory Drill")
         self.master.protocol("WM_DELETE_WINDOW",lambda: close(self))
         load_gui(self)
@@ -30,16 +29,6 @@ def load_gui(self):
     self.btn_add.grid(row=3,column=0,rowspan=1,columnspan=2,padx=(25,25),pady=(20,10),sticky=N)
     self.txt_bro1 = tk.Entry(self.master,text="")
     self.txt_bro1.grid(row=3,column=1,rowspan=1,columnspan=4,ipadx=(100),padx=(150,0),pady=(20,10),sticky=W+E)
-
-
-
-def center_window(self,w,h):
-    screen_width = self.master.winfo_screenwidth()
-    screen_height = self.master.winfo_screenheight()
-    x = int((screen_width/2) - (w/2))
-    y = int((screen_height/2) - (h/2))
-    center = self.master.geometry('{}x{}+{}+{}'.format(w,h,x,y))
-    return center
 
 
 def close(self):
